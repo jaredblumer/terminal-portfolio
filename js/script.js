3a07cb9ab916397b712f4ddb16d160b093770a4d
Jared Blumer <jaredgblumer@gmail.com>
2021-09-26 11:13:05 -0400

    Update aboutme, design, and links commands

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ $(function() {
     $('#terminal').terminal({
       aboutme: function () {
          this.echo('[[b;;]ABOUT ME]'
-         + "\n       I am a versatile Software Engineer and Data Analyst experienced in data "
-         + "\n       analytics, database management, graphic design, and full-stack web development, "
-         + "\n       specializing in Javascript, TypeScript, Node.js, React, and Python. I am the "
-         + "\n       Lead Organizer of Pittsburgh's node.pgh Meetup and a Volunteer Technical "
-         + "\n       Contributor for the Wikimedia Foundation."
+         + "\n       Hi there, my name's Jared Blumer ( he / him ). I am a Software Engineer and Data Analyst"
+         + "\n       experienced in data analytics, database management, graphic design, and full-stack web development."
+         + "\n       I specialize in Javascript, TypeScript, Node.js, Express.js, React, and Python and am the Lead"
+         + "\n       Organizer of Pittsburgh's node.pgh Meetup and a Volunteer Technical Contributor for the Wikimedia"
+         + "\n       Foundation."
+         + "\n"
+         + "\n       I am a lifelong learner and a proponent of [[!;;;;https://blog.doist.com/deep-work/]Deep Work] and the [[!;;;;https://fs.blog/2015/03/carol-dweck-mindset/]Growth Mindset], and believe (as stated"
+         + '\n       by Stanford psychologist Carol Dweck) that ' + '"effort is what makes you smart or talented."'
+         + "\n"
+         + "\n       Enter 'projects' to see what I'm working on to further develop my skills and knowledge."
          );
       },
       contact: function () {
@@ -17,7 +22,16 @@ $(function() {
       },
       design: function () {
          this.echo('[[b;;]DESIGN]'
-         + '\n       aboutme'
+         + '\n       WAHBI HUSSAIN'
+         + '\n       Contracted by Data Engineer Wahbi Hussain to design and build his portfolio site.'
+         + '\n       Utilized HTML5, CSS3, and JQuery. '
+         + '\n       View: https://wahbihussain.me/'
+         + '\n'
+         + '\n       ATZENI DESIGN STUDY'
+         + "\n       Inspired by Alessio Atzeni's portfolio site, I reverse engineered his website design"
+         + '\n       to improve my craft. Utilized HTML5, CSS3, JavaScript, and JQuery.'
+         + '\n       View: https://jaredblumer.github.io/atzeni-study/'
+         + '\n       Code: https://github.com/jaredblumer/atzeni-study'
          );
       },
       help: function () {
@@ -33,9 +47,9 @@ $(function() {
       },
       links: function () {
          this.echo('[[b;;]LINKS]'
+         + '\n       LinkedIn: https://www.linkedin.com/in/jaredblumer'
          + '\n       GitHub: https://github.com/jaredblumer'
          + '\n       GitLab: https://gitlab.com/jaredblumer'
-         + '\n       LinkedIn: https://www.linkedin.com/in/jaredblumer'
          );
       },
       opensource: function () {
