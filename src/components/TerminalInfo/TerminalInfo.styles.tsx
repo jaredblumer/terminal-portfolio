cac98d3b4fab9c79a3fe077e1b781fa2f654d813
jaredblumer <jaredblumer@protonmail.com>
2025-05-10 17:03:25 -0400

    Style terminal prompt

--- src/components/TerminalInfo/TerminalInfo.styles.tsx
+++ src/components/TerminalInfo/TerminalInfo.styles.tsx
@@ -5,3 +5,11 @@ import styled from 'styled-components';
 export const Label = styled.label`
     color: #99DF4D
 `;
+
+export const EmDash = styled.span`
+    color: #535e87
+`;
+
+export const PromptSymbol = styled.span`
+    color: #fff;
+`;
--- src/components/TerminalInfo/TerminalInfo.tsx
+++ src/components/TerminalInfo/TerminalInfo.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Label } from './TerminalInfo.styles';
+import { EmDash, Label, PromptSymbol } from './TerminalInfo.styles';
 
 export default function TerminalInfo() {
     return (
         <Label htmlFor='terminal-input'>
-            user@terminal.jaredblumer.dev:~$&nbsp;
+            user@terminal.jaredblumer.dev<PromptSymbol>:</PromptSymbol><EmDash>~</EmDash><PromptSymbol>$</PromptSymbol>&nbsp;
         </Label>
     )
 }
