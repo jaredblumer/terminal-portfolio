08fab2cc6532ca51950becefe8a8be1e890e9cdb
jaredblumer <jaredblumer@protonmail.com>
2025-05-17 12:30:16 -0400

    Update ts-jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -19,12 +19,9 @@ const config: Config = {
     ],
     testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    globals: {
-        'ts-jest': {
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
             tsconfig: 'tsconfig.jest.json',
-        },
+        }],
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
