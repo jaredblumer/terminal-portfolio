ede32775d93389cb05b9e75ca703650b538ab104
Jared Blumer <jaredblumer@protonmail.com>
2025-09-15 20:06:34 -0400

    Use next public environment variable and explicitly send ReactGA event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,39 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import ReactGA from "react-ga4";
+import ReactGA from 'react-ga4';
 import Terminal from '@/components/Terminal/Terminal';
 import { Ubuntu_Mono } from 'next/font/google';
 
-const gaId = process.env.GA_MEASUREMENT_ID;
+const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
 if (gaId) {
   ReactGA.initialize(gaId);
 } else {
-  console.warn("Google Analytics measurement ID is missing");
+  console.warn('Google Analytics measurement ID is missing');
 }
 
-const ubuntuMono = Ubuntu_Mono({
-  weight: '400',
-  subsets: ['latin'],
-})
+const ubuntuMono = Ubuntu_Mono({ weight: '400', subsets: ['latin'] });
 
 export default function App() {
-
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     setIsLoaded(true);
   }, []);
 
-  return (
-    isLoaded ? (
-      <div id="container" className={ubuntuMono.className}>
-        <Terminal />
-      </div>
-    ) : (
-      null
-    )
-  );
+  useEffect(() => {
+    if (!gaId) return;
+    ReactGA.send({
+      hitType: 'pageview',
+      page: window.location.pathname,
+      title: document.title,
+    });
+  }, []);
+
+  return isLoaded ? (
+    <div id="container" className={ubuntuMono.className}>
+      <Terminal />
+    </div>
+  ) : null;
 }
